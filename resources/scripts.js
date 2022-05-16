166cdbaa46512b9c066b25d740e6e600f62b5a80
realdashiedash <31542077+dashiedash@users.noreply.github.com>
2022-05-16 17:49:59 +0800

    Amend script to make the code work.

--- resources/scripts.js
+++ resources/scripts.js
@@ -1,6 +1,10 @@
 // Make the nav bar stick on top when the page is scrolled.
 
-let nav = document.getElementById("navbar");
+window.onscroll = function () {
+  makeNavStick();
+};
+
+let nav = document.getElementById("nav");
 
 let sticky = nav.offsetTop;
 
@@ -10,16 +14,14 @@ console.log(sticky);
 /*
 function makeNavStick() {
   if (window.scrollY >= sticky) {
-    nav.classList.add("sticky");
+    nav.classList.add("stickIt");
   } else {
-    nav.classList.remove("sticky");
+    nav.classList.remove("stickIt");
   }
 }
 */
 
 const makeNavStick = () =>
   window.scrollY >= sticky
-    ? nav.classList.add("sticky")
-    : nav.classList.remove("sticky");
-
-window.onscroll = makeNavStick();
+    ? nav.classList.add("stickIt")
+    : nav.classList.remove("stickIt");
