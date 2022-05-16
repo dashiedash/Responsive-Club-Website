ee671f5ab49cdaba061e8ed2e3a99bb751a35f4f
realdashiedash <31542077+dashiedash@users.noreply.github.com>
2022-05-16 18:16:57 +0800

    Add styles for About section.

--- resources/scripts.js
+++ resources/scripts.js
@@ -1,15 +1,7 @@
 // Make the nav bar stick on top when the page is scrolled.
-
-window.onscroll = function () {
-  makeNavStick();
-};
-
 let nav = document.getElementById("nav");
 
 let sticky = nav.offsetTop;
-
-console.log(nav);
-console.log(sticky);
 
 /*
 function makeNavStick() {
@@ -25,3 +17,7 @@ const makeNavStick = () =>
   window.scrollY >= sticky
     ? nav.classList.add("stickIt")
     : nav.classList.remove("stickIt");
+
+window.onscroll = function () {
+  makeNavStick();
+};
